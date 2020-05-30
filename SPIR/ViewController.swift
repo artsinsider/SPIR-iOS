dc8d675394b04d86c1dc5cb997afc9d0e3768e0f
Диденко Егор <didenko_e@tass.ru>
2020-05-30 16:37:52 +0300

    Initial Commit

---
+++ SPIR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SPIR
+//
+//  Created by Egor on 30.05.2020.
+//  Copyright © 2020 TASS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
