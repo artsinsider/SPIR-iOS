cc6262814acc1dcaaf81f6f5682f9a264cde2fe2
Alexander Trifonov <fgengine@gmail.com>
2020-08-13 20:17:56 +0300

    Quickly #3

--- SPIR/Screen/Feed/Cell/Content/FeedScreenContentCell.swift
+++ SPIR/Screen/Feed/Cell/Content/FeedScreenContentCell.swift
@@ -5,14 +5,13 @@
 import Quickly
 
 class FeedScreenContentRow : QTableRow {
+    
     let feed: IFeed
     
     init( feed: IFeed ) {
         self.feed = feed
         super.init(canSelect: true)
     }
-    
-    
     
 }
 
@@ -21,12 +20,11 @@ class FeedScreenContentCell : QTableCell< FeedScreenContentRow > {
     @IBOutlet weak var headLable: UILabel!
     @IBOutlet weak var leadLable: UILabel!
     
-    override func set(row: RowType, spec: IQContainerSpec, animated: Bool) {
-        super.set(row: row, spec: spec, animated: animated)
+    override func prepare(row: RowType, spec: IQContainerSpec, animated: Bool) {
+        super.prepare(row: row, spec: spec, animated: animated)
+        
         self.headLable.text = row.feed.headLine
         self.leadLable.text = row.feed.lead
-        
-       }
-
+    }
     
 }
